e5f64ef4611e63e984ecd22bbd68af8f60f79488
madihamamdouh <69203448+madihamamdouh@users.noreply.github.com>
2026-02-08 15:24:16 +0400

    add movie repo

---
+++ src/test/java/dev/madiham/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.madiham.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
